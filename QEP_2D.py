094097dab03bc5996d6c6f837af879ecbbb80702
psalasiya <prasannasalasiya@gmail.com>
2025-09-09 11:18:22 -0500

    made changes to make more user-friendly

--- QEP_2D.py
+++ QEP_2D.py
@@ -1,13 +1,14 @@
 #%%
 from solver_QEP_2D import *
 from geo_mat_mesh_2D_QEP import *
-from evs_class import *
 import multiprocessing as mp
 from functools import partial
 from scipy.io import savemat
+import numpy as np
+import pickle
 
 #%% Function
-def QEP(ky, a1, a2, msh, rh, mu, omega, num_ev):
+def QEP(ky, a1, a2, msh, rh, mu, omega):
     """
     Main function to run a single QEP simulation for a given ky value.
 
@@ -33,57 +34,17 @@ def QEP(ky, a1, a2, msh, rh, mu, omega, num_ev):
     ############################ QEP solver ####################################
     # Call the main solver function
     ev, ef = QEP_solver_2D(a1, a2, msh, rh, mu, omega, ky, Z=1, num_itr = 200, feorder = 3) # Z=1 is a placeholder, num_itr is number of Arnoldi iterations, feorder is FE element order
-
-    ########################## post-processing ##################################
-    # Create an eigenspectrum object to handle filtering
-    evs = eigenspectrum(ev, ef)
-    # Select modes within the first Brillouin Zone
-    evs.pick_first_BZ()
-
-    ##################### lattice-properties ####################################
-    # Initialize skew coordinate system handler
-    lattice = skew(a1, a2)
-    # Compute transformation matrix for coordinate mapping
-    lattice.lattice_to_cart_coeff()
-
-    ########################### MATLAB readable form #############################
-    # This section interpolates the NGSolve GridFunction solution onto a regular
-    # Cartesian grid, and then maps to skew coordinates that is easier to work with in MATLAB.
-    inter_x = 101 # Number of interpolation points in x
-    inter_y = 101 # Number of interpolation points in y
-
-    x, y = np.meshgrid(np.linspace(0, 1, inter_x), np.linspace(0, 1, inter_y)) # local coordinates
-    u = lattice.C[0, 0]*x + lattice.C[0, 1]*y
-    v = lattice.C[1, 0]*x + lattice.C[1, 1]*y  # global coordinates
-
-    # Initialize arrays to store the interpolated data
-    kappa = np.zeros(len(evs.ev_BZ), dtype=complex)                   # eigenvalue
-    phi = np.zeros((inter_y, inter_x, len(evs.ev_BZ)), dtype=complex) # eigenfunction
-    G = np.zeros((inter_y, inter_x))                                # shear modulus
-
-    # Loop through the selected modes and evaluate them at each grid point
-    for k in range(len(evs.ev_BZ)):
-        temp_phi = evs.ef_BZ[k]
-        kappa[k] = evs.ev_BZ[k]
-
-        for i in range(inter_y):
-            for j in range(inter_x):
-                phi[i, j, k] = temp_phi(msh(u[i, j], v[i, j]))
-                G[i, j]  = mu(msh(u[i, j], v[i, j]))  
     
-    return x, y, phi, G, kappa, ky
+    return ev, ef, ky, msh, rh, mu
 
 #%% Input parameters for the simulation sweep
-omega = 1.95
+omega = 1.4
 # Define the range of ky values to sweep over
-ky = np.linspace(0, 2, 100) * np.pi
-
-# Set the maximum number of eigenvalues/modes to save for each ky
-num_ev = 9 # maximum number of eigenvalues
+ky = np.linspace(1.7, 1.8, 100) * np.pi
 
 # Define the lattice vectors for the unit cell
 a1 = np.array([1, 0]) # lattice vector 1
-a2 = np.array([1, 1]) # lattice vector 2
+a2 = np.array([1, 2]) # lattice vector 2
 
 # Define the unit cell geometry
 msh, rh, mu  = build_skew_cell(a1, a2)
@@ -91,18 +52,10 @@ msh, rh, mu  = build_skew_cell(a1, a2)
 #%% Parallelising
 if __name__ == "__main__":
     with mp.Pool(processes=mp.cpu_count()) as pool:
-        results = pool.map(partial(QEP, a1 = a1, a2 = a2, msh = msh, rh = rh, mu = mu, omega = omega, num_ev = num_ev), ky)
+        results = pool.map(partial(QEP, a1 = a1, a2 = a2, msh = msh, rh = rh, mu = mu, omega = omega), ky)
 
     for i in range(len(ky)):
-        matfile_s    = 'Matlab_data/ky_'+str(i)+".mat"
-        mdict = {'x' : results[i][0], 'y' : results[i][1], 'phi' : results[i][2], 'G':results[i][3], 'kappa' : results[i][4], 'ky' : results[i][5]}
-        print("num_ev = " + str(len(results[i][4])), ", ind = " + str(i+1))
-        savemat(matfile_s, mdict)
-
-# %% 
-# for i in range(len(ky)):
-#     results = QEP(ky[i], a1, a2, omega, num_ev)
-#     matfile_s    = 'Matlab_data/ky_'+str(i)+".mat"
-#     mdict = {'x' : results[i][0], 'y' : results[i][1], 'phi' : results[i][2], 'G':results[i][3], 'kappa' : results[i][4], 'ky' : results[i][5]}
-#     print("num_ev = " + str(len(results[i][4])), ", ind = " + str(i+1))
-#     savemat(matfile_s, mdict)
+        my_tuple = (results[i][0], results[i][1], results[i][2], results[i][3], results[i][4], results[i][5])
+        file_s = 'Data/ky_'+str(i)
+        with open(file_s, "wb") as picklefile:
+            pickle.dump(my_tuple, picklefile)
--- solver_QEP_2D.py
+++ solver_QEP_2D.py
@@ -1,6 +1,5 @@
 from ngsolve import *
 import numpy as np
-from skew_class import *
 
 def QEP_solver_2D(a1, a2, mesh, rh, mu, omega, ky, Z, num_itr, feorder):
     """
@@ -42,18 +41,23 @@ def QEP_solver_2D(a1, a2, mesh, rh, mu, omega, ky, Z, num_itr, feorder):
     u, s = fes.TrialFunction()
     v, h = fes.TestFunction()
     
-    # Initialize the skew lattice coordinate system handler.
-    lattice = skew(a1, a2)
-    # Compute the contravariant metric tensor for gradient calculations in skew coordinates.
-    lattice.metric_contravariant()
+    # Contravariant vectors
+    b1, b2 = contravariant(a1, a2)
+    
+    metric_contravariant(b1, b2)
     # Compute the transformation matrix for NGSolve's partial derivatives.
-    lattice.grad_cart_to_lattice_coeff()
+    grad_cart_to_lattice_coeff(a1, a2)
 
     # Extract components of the contravariant metric tensor G^ij.
-    G11 = lattice.Gcon[0, 0]
-    G12 = lattice.Gcon[0, 1]
-    G21 = lattice.Gcon[1, 0]
-    G22 = lattice.Gcon[1, 1]
+    # Compute the contravariant metric tensor for gradient calculations in skew coordinates.
+    Gcon = metric_contravariant(b1, b2)
+    G11 = Gcon[0, 0]
+    G12 = Gcon[0, 1]
+    G21 = Gcon[1, 0]
+    G22 = Gcon[1, 1]
+
+    # Gradient matrix
+    D = grad_cart_to_lattice_coeff(a1, a2)
 
     # --- Linearization of the QEP ---
     # The QEP (A*kx² + B*kx + C)u = 0 is converted to a linear system:
@@ -64,23 +68,23 @@ def QEP_solver_2D(a1, a2, mesh, rh, mu, omega, ky, Z, num_itr, feorder):
     # Bilinear form for the left-hand side matrix of the linear system.
     a = BilinearForm(fes)
     a += omega ** 2 * rh * u * v * dx
-    a += -mu * G11 * partial(u, "x1", lattice.D) * partial(v, "x1", lattice.D) * dx
-    a += -mu * G12 * partial(u, "x1", lattice.D) * partial(v, "x2", lattice.D) * dx
-    a += -mu * G21 * partial(u, "x2", lattice.D) * partial(v, "x1", lattice.D) * dx
-    a += -mu * G22 * partial(u, "x2", lattice.D) * partial(v, "x2", lattice.D) * dx
+    a += -mu * G11 * partial(u, "x1", D) * partial(v, "x1", D) * dx
+    a += -mu * G12 * partial(u, "x1", D) * partial(v, "x2", D) * dx
+    a += -mu * G21 * partial(u, "x2", D) * partial(v, "x1", D) * dx
+    a += -mu * G22 * partial(u, "x2", D) * partial(v, "x2", D) * dx
     a += -G22 * ky**2 * mu * u * v  * dx
-    a += G21 * 1j * mu * (partial(u, "x1", lattice.D) * v - u * partial(v, "x1", lattice.D)) * ky  * dx
-    a += G22 * 1j * mu * (partial(u, "x2", lattice.D) * v - u * partial(v, "x2", lattice.D)) * ky  * dx
-    a += -G12 * 1j * mu * s * partial(v, "x2", lattice.D)  * dx
-    a += -G11 * 1j * mu * s * partial(v, "x1", lattice.D) * dx
+    a += G21 * 1j * mu * (partial(u, "x1", D) * v - u * partial(v, "x1", D)) * ky  * dx
+    a += G22 * 1j * mu * (partial(u, "x2", D) * v - u * partial(v, "x2", D)) * ky  * dx
+    a += -G12 * 1j * mu * s * partial(v, "x2", D)  * dx
+    a += -G11 * 1j * mu * s * partial(v, "x1", D) * dx
     a += Z * s * h * dx
 
     # Bilinear form for the right-hand side matrix of the linear system.
     b = BilinearForm(fes)
     b += G11 * mu * s * v * dx
     b += 2*G12 * ky * mu * u * v * dx
-    b += -G12 * 1j * mu * partial(u, "x2", lattice.D) * v * dx
-    b += -G11 * 1j * mu * partial(u, "x1", lattice.D) * v * dx
+    b += -G12 * 1j * mu * partial(u, "x2", D) * v * dx
+    b += -G11 * 1j * mu * partial(u, "x1", D) * v * dx
     b += Z * u * h * dx
 
     # Assemble the matrices.
@@ -196,3 +200,63 @@ def partial(fun, coord, D):
         dfun = grad(fun) * (D[1, 0], D[1, 1])
 
     return dfun
+
+def contravariant(a1, a2):
+     """
+     Computes the contravariant vectors to a_1, a_2 s.t. a_i . b^j = delta_{ij}
+     """
+     J = np.column_stack((a1, a2))
+
+     # Reciprocal lattice vectors are columns of (J^-1)^T
+     B = np.linalg.inv(J).T
+     b1 = B[:, 0]
+     b2 = B[:, 1]
+
+     return b1, b2
+
+def metric_covariant(a1, a2):
+        """
+        Computes the covariant metric tensor G_ij = a_i . a_j.
+        This tensor is used for operations involving covariant vector components.
+        """
+        Gcov = np.array([
+            [np.dot(a1, a1), np.dot(a1, a2)],
+            [np.dot(a2, a1), np.dot(a2, a2)]
+        ])
+
+        return Gcov
+
+def metric_contravariant(b1, b2):
+        """
+        Computes the contravariant metric tensor G^ij = b^i . b^j.
+        This tensor is essential for calculating vector magnitudes and dot products
+        from contravariant components, such as those of the gradient.
+        """
+        Gcon = np.array([
+            [np.dot(b1, b1), np.dot(b1, b2)],
+            [np.dot(b2, b1), np.dot(b2, b2)]
+        ])
+
+        return Gcon
+
+def grad_cart_to_lattice_coeff(a1, a2):
+        """
+        Computes the transformation matrix 'D' for converting the components
+        of a Cartesian gradient (∂f/∂z1, ∂f/∂z2) to lattice gradient components
+        (∂f/∂x1, ∂f/∂x2) used in NGSolve's partial derivatives.
+        
+        NGSolve's `grad(u)` is in Cartesian coordinates. `partial(u, "x1", D)`
+        transforms it.
+        """
+
+        return np.column_stack((a1, a2)).T
+
+def lattice_to_cart_coeff(a1, a2):
+        """
+        Computes the transformation matrix 'C' for converting lattice
+        position coordinates (x, y) to Cartesian coordinates (u1, u2).
+        
+        [u1, u2]^T = C @ [x, y]^T
+        """
+
+        return np.column_stack((a1, a2))
